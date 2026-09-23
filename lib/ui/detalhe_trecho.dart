0d63c61451e908c77adba3d379d8b6a7796a4a80
camussi42 <camussireborn@gmail.com>
2026-09-23 19:43:20 -0300

    conserta o import de validadeRelatoPadrao no detalhe do trecho

--- lib/ui/detalhe_trecho.dart
+++ lib/ui/detalhe_trecho.dart
@@ -2,6 +2,7 @@
 library;
 
 import 'package:achar_vagas_app/models/estado_trecho.dart';
+import 'package:achar_vagas_app/models/relato.dart';
 import 'package:achar_vagas_app/ui/camadas_mapa.dart';
 import 'package:achar_vagas_app/ui/paleta_estado.dart';
 import 'package:flutter/material.dart';
